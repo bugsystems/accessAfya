b45fad5297a46854a39c3dbc6146bb165289b375
MARASI KEPHA <thecloud79@gmail.com>
2020-03-31 21:01:27 +0300

    Update App.js
    Live Page from git

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <Router>
         <div className='App'>
+<h4>Analytics Live GitHub Page</>
           <Metrics />
         </div>
       </Router>
